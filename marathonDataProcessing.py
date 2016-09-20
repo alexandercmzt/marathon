a18a328b294c7d08974d4aa6bfe334573e5505b3
Caitrin Armstrong <caitrin.armstrong@mail.mcgill.ca>
2016-09-20 02:35:20 -0400

    yay data'

---
+++ marathonDataProcessing.py
@@ -0,0 +1,215 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import csv
+from collections import defaultdict
+import datetime
+import re
+import numpy as np
+import pickle
+
+
+# In[2]:
+
+participants = dict()
+
+
+# In[3]:
+
+with open('docs/Project1_data.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    reader.next()
+    for row in reader:
+        participants[row[0]] = [row[i:i + 5] for i in xrange(1, len(row), 5)]
+
+
+# In[ ]:
+
+#outputs for clarity's sake. 2d lists, will be converted to numpy arrays
+participantDataForRaceParticipation = list()
+participantDataForRaceTimes = list() #without those who did not participate in 2015
+montrealMarathonParticipaton = list()
+montrealMarathonTime = list()
+participantDataFinalFor2016 = list() #years shifted by one. So including 2015, excluding 2012.
+
+
+#processing dictionaries
+marathonTotalTime = defaultdict(lambda: list())
+marathonAverageTime = dict()
+
+
+# In[ ]:
+
+for raceList in participants.values():
+    for race in raceList:
+        if "marathon" in race[1].lower():
+            if any(word in race[2].lower() for word in ["demi","half"]):
+                raceName = race[1] + " demi"
+            else:
+                raceName = race[1]           
+            try:
+                finishTime = datetime.datetime.strptime(race[3], "%H:%M:%S")
+                finishSeconds = finishTime.second+finishTime.minute*60+finishTime.hour*3600
+            except:
+                finishSeconds = 0
+            marathonTotalTime[raceName].append(finishSeconds)
+
+for race in marathonTotalTime:
+    marathonTotalTime[race] = [max(marathonTotalTime[race]) if (x == -1) else x for x in marathonTotalTime[race]]
+for race in marathonTotalTime:
+    marathonAverageTime[race] = sum(marathonTotalTime[race])/len(marathonTotalTime[race])
+
+def getFinishTime(finishTime, raceName):
+    try:
+        finishTime = datetime.datetime.strptime(finishTime, "%H:%M:%S")
+        finishSeconds = finishTime.second+finishTime.minute*60+finishTime.hour*3600
+    except:
+        finishSeconds = max(marathonTotalTime[raceName])
+    
+    return finishSeconds
+
+def processAge(givenAge):
+    pattern1 = re.compile("^[A-Z][0-9]{2}\-[0-9]{2}$")
+    pattern2 = re.compile("^[A-Z][0-9]{2}")
+    pattern3 = re.compile("^[A-Z]\s[0-9]{2}\-[0-9]{2}$")
+    pattern4 = re.compile("^M|H")
+    pattern5 = re.compile("^F")
+    if pattern1.match(givenAge):
+        gender = givenAge[0]
+        age = (int(givenAge[1:3]) + int(givenAge[4:6]))/2
+    elif pattern2.match(givenAge):
+        gender = givenAge[0]
+        age = (int(givenAge[1:3]))
+    elif pattern3.match(givenAge):
+        gender = givenAge[0]
+        age = (int(givenAge[2:4]) + int(givenAge[5:7]))/2
+    elif pattern4.match(givenAge):
+        gender = "M"
+        age = None
+    elif pattern5.match(givenAge):
+        gender = "F"
+        age = None
+    else:
+        gender = None
+        age = None
+
+    if gender == "F":
+        gender = 0
+    if gender == "M":
+        gender = 1
+
+    return age, gender
+
+training = True
+for participantNumber, raceList in participants.items():
+    year2012, year2013, year2014, year2015, year2016 = (0,)*5
+    gender = 1
+    age = 30
+    distanceFromAverage = 0
+    numberOfMarathons = 0
+    numberOfMontrealMarathons = 0
+    attendedMontreal2015 = False
+    totalNumberOfAgedRaces = 0
+    montreal2015FinishTime = 0
+    for race in raceList:
+        if "2012" in race[0]:
+            year2012 += 1
+        elif "2013" in race[0]:
+            year2013 += 1
+        elif "2014" in race[0]:
+            year2014 += 1
+        elif "2015" in race[0]:
+            year2015 += 1
+            if "Marathon Oasis Rock 'n' Roll de Montreal" in race[1].lower() and "demi" not in race[2].lower:
+                raceName = race[1]
+                attendedMontreal2015 = True
+                montreal2015FinishTime = getFinishTime(race[3], raceName)
+                if training: #if this is the training set their time is the average time.
+                    year2015 -= 1
+                    distanceFromAverage = marathonAverageTime[raceName]
+                    continue                    
+        elif "2016" in race[0]:
+            year2016 += 1       
+        
+        raceAge, potentialGender = processAge(race[4])        
+        if raceAge is not None:
+            age += raceAge
+            totalNumberOfAgedRaces += 1     
+        if potentialGender is not None:
+            gender = potentialGender
+
+        if "marathon" in race[1].lower():
+            numberOfMarathons += 1
+            if "montreal" in race[1].lower():
+                numberOfMontrealMarathons += 1
+            if any(word in race[2].lower() for word in ["demi","half"]):
+                raceName = race[1] + " demi"
+            else:
+                raceName = race[1]
+            finishSeconds = getFinishTime(race[3], raceName) 
+            distanceFromAverage += finishSeconds - marathonAverageTime[raceName]
+
+    ###NOW RETURN ARRAYS### 
+    distanceFromAverageAverage = distanceFromAverage/numberOfMarathons
+    if totalNumberOfAgedRaces != 0:
+        age = age/totalNumberOfAgedRaces 
+    participantNumber = int(participantNumber)
+    
+    if training:
+        participantDataForRaceParticipation.append([
+                participantNumber, 
+                year2012, 
+                year2013, 
+                year2014, 
+                year2015,
+                gender,
+                age,
+                numberOfMontrealMarathons,
+                distanceFromAverageAverage
+                ])
+        if attendedMontreal2015:
+            participantDataForRaceTimes.append([
+                    participantNumber, 
+                    year2012, 
+                    year2013, 
+                    year2014, 
+                    year2015,
+                    gender,
+                    age,
+                    numberOfMontrealMarathons,
+                    distanceFromAverageAverage
+                ])
+
+        if attendedMontreal2015:
+            montrealMarathonParticipaton.append([participantNumber, 1])
+            montrealMarathonTime.append([participantNumber], montreal2015FinishTime)
+        else:
+            montrealMarathonParticipaton.append([participantNumber, 0])
+
+    if not training:
+        participantDataFinalFor2016.append([
+                    participantNumber, 
+                    year2013, 
+                    year2014, 
+                    year2015,
+                    year2016,
+                    gender,
+                    age,
+                    numberOfMontrealMarathons,
+                    distanceFromAverageAverage
+                ])
+        
+pickle.dump(np.array(participantDataFinalFor2016), open('final_participantDataFinalFor2016.p', 'wb'))
+pickle.dump(np.array(participantDataForRaceParticipation), open('X_participantDataForRaceParticipation.p', 'wb'))
+pickle.dump(np.array(participantDataForRaceTimes), open('X_participantDataForRaceTimes.p', 'wb'))
+pickle.dump(np.array(montrealMarathonParticipaton), open('Y_montrealMarathonParticipaton.p', 'wb'))
+pickle.dump(np.array(montrealMarathonTime), open('Y_montrealMarathonTime.p', 'wb'))
+
+
+
+# In[ ]:
+
+
+
