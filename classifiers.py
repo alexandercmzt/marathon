0c70bab553365bf8667cd272f069ca27b5a7962a
Keith Strickling <kstrickling@cloudops.com>
2016-09-21 17:29:57 -0400

    changes

--- classifiers.py
+++ classifiers.py
@@ -54,10 +54,10 @@ class LinearRegressor:
     def gradient_descent(self,X, y):
     	current_best = self.cost(self.X, self.y, self.w)
     	cost_history = []
-    	for _ in xrange(1000):
+    	for _ in xrange(10000):
     		self.w = self.w - self.alpha*self.deriv_cost(self.X, self.y, self.w)
     		this_cost = self.cost(self.X, self.y, self.w)
-    		if abs(current_best - this_cost) < 0.001:
+    		if abs(current_best - this_cost) < 0.1:
     			cost_history.append(this_cost)
     			return self.w, cost_history
     		else:
@@ -65,9 +65,9 @@ class LinearRegressor:
     			current_best = this_cost
     			if len(cost_history) > 4:
     				if cost_history[-1] > cost_history[-2] and cost_history[-2] > cost_history[-3] and cost_history[-3] > cost_history[-4]:
-    					print "PROBLEM IN GRADIENT DESCENT: COST HAS INCREASED FOR 4 CONSECUTIVE ITERATIONS, ALPHA IS TOO LARGE"
+    					print "ALPHA TOO LARGE"
     					return self.w, cost_history
-    	print "PROBLEM IN GRADIENT DESCENT:  NO CONVERGENCE AFTER 1000 ITERATIONS, ALPHA IS TOO SMALL"
+    	print "ALPHA TOO SMALL"
     	return self.w, cost_history
 
     def train(self,gd=True):
@@ -102,7 +102,7 @@ class LogisticRegressor():
     	self.w = np.random.rand(self.n+1)
 
     def sigmoid(self,x):
-    		return 1/(1+np.exp(-x))
+        return 1/(1+np.exp(-x))
 
     def predict(self,X):
     	try:
@@ -117,17 +117,18 @@ class LogisticRegressor():
     def cost(self, X, y, w=None):
         if w == None:
             w = self.w
-    	prediction = self.predict(X)
-    	return -(1/float(X.shape[0]))*(y.T.dot(np.log(prediction)) + (1-y).T.dot(np.log(1-prediction)))
+        prediction = self.predict(X)
+        return -(1/float(X.shape[0]))*np.sum(-y.T.dot(np.log(prediction)) - (1-y.T).dot(np.log(prediction)))
+        #return -(1/float(X.shape[0]))*(y.T.dot(np.log(prediction)) + (1-y).T.dot(np.log(1-prediction)))
 
     def deriv_cost(self, X, y, w):
-    	prediction = self.predict(X)
-    	return X.T.dot(prediction-y)
+        prediction = self.predict(X)
+        return X.T.dot(prediction-y)
 
     def gradient_descent(self,X, y):
     	current_best = self.cost(self.X, self.y, self.w)
     	cost_history = []
-    	for _ in xrange(1000):
+    	for _ in xrange(10000):
     		self.w = self.w - self.alpha*self.deriv_cost(self.X, self.y, self.w)
     		this_cost = self.cost(self.X, self.y, self.w)
     		if abs(current_best - this_cost) < 0.001:
@@ -138,16 +139,17 @@ class LogisticRegressor():
     			current_best = this_cost
     			if len(cost_history) > 4:
     				if cost_history[-1] > cost_history[-2] and cost_history[-2] > cost_history[-3] and cost_history[-3] > cost_history[-4]:
-    					print "PROBLEM IN GRADIENT DESCENT: COST HAS INCREASED FOR 4 CONSECUTIVE ITERATIONS, ALPHA IS TOO LARGE"
+    					print "ALPHA IS TOO LARGE"
     					return self.w, cost_history
-    	print "PROBLEM IN GRADIENT DESCENT:  NO CONVERGENCE AFTER 1000 ITERATIONS, ALPHA IS TOO SMALL"
+    	print "ALPHA IS TOO SMALL"
     	return self.w, cost_history
 
     def train(self):
     	wts, cost_history = self.gradient_descent(self.X,self.y)
-    	#print "COST_HISTORY: " + str(cost_history)
+    	print "COST_HISTORY: " + str(cost_history)
+        raw_input()
     	print "Ended after " + str(len(cost_history)) + " iterations"
-    	print "Optimal weights: " + str(wts)
+    	#print "Optimal weights: " + str(wts)
     	return cost_history
 
 
